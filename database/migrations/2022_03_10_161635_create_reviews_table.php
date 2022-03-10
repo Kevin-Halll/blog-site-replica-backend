8ec7f029376ba0d718db04faa223e82d3e7bf880
rdwayne876 <rdwayne876@gmail.com>
2022-03-10 12:57:29 -0500

    Create reviews table

---
+++ database/migrations/2022_03_10_161635_create_reviews_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('company_id');
+            $table->float('star_rating');
+            $table->string('title');
+            $table->text('review');
+            $table->integer('helpful');
+            $table->integer('not_helpful');
+            $table->date('updated_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+};
