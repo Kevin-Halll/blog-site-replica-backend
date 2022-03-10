7077b5b925384f9a67df2dea4e8ec6946142ca17
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-03-10 12:55:22 -0500

    created addresses migration

---
+++ database/migrations/2022_03_10_175310_create_addresses_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('is_company');
+            $table->integer('company_user_id');
+            $table->text('address_line_1');
+            $table->text('address_line_2');
+            $table->string('city');
+            $table->string('parish');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
