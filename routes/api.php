3a43575a5150c4524facf4a4c6736e36e6b9faed
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-03-14 18:05:48 -0500

    created crud for companies

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\CompanyController;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('/company', [CompanyController::class, 'index']);
+Route::get('/company/{id}', [CompanyController::class, 'show']);
+Route::post('/company', [CompanyController::class, 'store']);
+Route::put('/company/{id}', [CompanyController::class, 'update']);
+Route::get('/company/{id}/deactivate', [CompanyController::class, 'deactivate']);
+Route::get('/company/{id}/restore', [CompanyController::class, 'restore']);
+Route::delete('/company/{id}', [CompanyController::class, 'destroy']);
