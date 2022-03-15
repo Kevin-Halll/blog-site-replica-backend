06d8bf308c83c3b4b1fc99033328acdbd14df63d
rdwayne876 <rdwayne876@gmail.com>
2022-03-15 10:13:54 -0500

    api protected user routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\UserController;
+use GuzzleHttp\Middleware;
+use Illuminate\Auth\Middleware\Authenticate;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +20,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(["middleware" => ['auth:sanctum']], function(){
+    Route::get("/user", [AuthenticatedSessionController::class, 'get']);
+    Route::put("/update/{id}", [UserController::class, 'update']);
 });
