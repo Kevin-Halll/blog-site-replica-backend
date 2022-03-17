f810195f5bb4420b959f4b55ad0a8471713c5eed
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-03-17 09:19:06 -0500

    Updated seeders for all tables

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Review extends Model
 {
     use HasFactory, HasApiTokens;
+
+    use SoftDeletes;
 
     protected $fillable = [
         'user_id',
--- database/seeders/ReviewSeeder.php
+++ database/seeders/ReviewSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Review;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,40 @@ class ReviewSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Review::create(
+            [
+            'user_id' => 1,
+            'company_id' => 1,
+            'star_rating' => rand(1, 5),
+            'title' => "This is a review",
+            'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+            ]);
+
+        Review::create([
+            'user_id' => 1,
+            'company_id' => 2,
+            'star_rating' => rand(1, 5),
+            'title' => "This is a review",
+            'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        ]);
+
+        Review::create([
+           'user_id' => 2,
+           'company_id' => 1,
+           'star_rating' => rand(1, 5),
+           'title' => "This is a review",
+           'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+
+
+        ]);
+
+        Review::create([
+            'user_id' => 2,
+            'company_id' => 2,
+            'star_rating' => rand(1, 5),
+            'title' => "This is a review",
+            'content' => "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+
+        ]);
     }
 }
