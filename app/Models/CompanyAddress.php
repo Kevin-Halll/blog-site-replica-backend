582969fbe942f12258e1ac8be06051aa7522ca07
rdwayne876 <63379431+rdwayne876@users.noreply.github.com>
2022-03-18 15:56:39 -0500

    Added Passport Api tokens

--- app/Models/CompanyAddress.php
+++ app/Models/CompanyAddress.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class CompanyAddress extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ["company_id", "address_line_1", "address_line_2", "city", "parish"];
 }
--- app/Models/UserAddress.php
+++ app/Models/UserAddress.php
@@ -4,9 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Passport\HasApiTokens;
 
 class UserAddress extends Model
 {
-    use HasFactory;
+    use HasFactory, HasApiTokens;
     protected $fillable = ["user_id", "address_line_1", "address_line_2", "city", "parish"];
 }
