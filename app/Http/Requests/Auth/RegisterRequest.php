b03f1d222d36ac9dd55e78bdc70cf41b3519129e
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2022-03-23 12:57:38 -0500

    added heruko procfile

--- app/Http/Requests/Auth/RegisterRequest.php
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -28,6 +28,7 @@ class RegisterRequest extends FormRequest
             'last_name' => ['required', 'string','max:255'],
             'email' => ['required','string','email','max:255','unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'dob' => ['required', 'string'],
             'phone' => ['required', 'string', 'max:17']
         ];
     }
